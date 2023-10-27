2464bc391f02c58b2ca474b129b9afe12a017339
Kain Nhantumbo <nhantumbok@gmail.com>
2023-10-27 17:50:38 +0200

    feat: added prettier config

---
+++ src/shared/reducer.ts
@@ -0,0 +1,11 @@
+import { actions } from './actions';
+import { Action, State } from '@/types';
+
+export const initialState: State = {};
+
+export function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    default:
+      return { ...state };
+  }
+}
